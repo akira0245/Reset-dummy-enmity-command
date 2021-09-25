5c525808c237329d1e1fff6fbb1bab1ab20e0dc8
akira0245 <dbkxiang@gmail.com>
2021-09-25 16:57:22 +0800

    api4

--- Plugin.cs
+++ Plugin.cs
@@ -1,48 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using Dalamud.Game.ClientState.Actors.Types.NonPlayer;
+using Dalamud.Game;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Logging;
 using Dalamud.Plugin;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using BattleChara = Dalamud.Game.ClientState.Objects.Types.BattleChara;
+using Character = Dalamud.Game.ClientState.Objects.Types.Character;
 
-namespace Reset_dummy_enmity_command
+namespace ResetEnmityCommand
 {
-    public class Plugin:IDalamudPlugin
-    {
-	    private DalamudPluginInterface pi;
-	    public void Dispose()
-	    {
-		    pi.CommandManager.RemoveHandler("/resetenmity");
-			pi.Dispose();
-	    }
+	public class Plugin : IDalamudPlugin
+	{
+		[PluginService] public static DalamudPluginInterface DalamudPluginInterface { get; private set; }
+		[PluginService] public static TargetManager TargetManager { get; private set; }
+		//[PluginService] public static ObjectTable ObjectTable { get; private set; }
+		[PluginService] public static GameGui GameGui { get; private set; }
+		[PluginService] public static CommandManager CommandManager { get; private set; }
+		[PluginService] public static SigScanner SigScanner { get; private set; }
 
-	    public void Initialize(DalamudPluginInterface pluginInterface)
-	    {
-		    pi = pluginInterface;
-		    IntPtr ptr = pi.TargetModuleScanner.ScanText("E8 ?? ?? ?? ?? 8D 43 0A");
-		    int offset = Marshal.ReadInt32(pi.TargetModuleScanner.ScanText("88 88 ?? ?? ?? ?? 48 8B 4B 08") + 2);
-		    PluginLog.Verbose($"ExecuteCommand: {ptr.ToInt64():X} offset: +{offset:X}");
-		    pi.CommandManager.AddHandler("/resetenmity", new CommandInfo((command, arguments) =>
-		    {
-			    unsafe
-			    {
-				    var ExecuteCommand = (delegate*<int, int, int, int, int, long>)ptr;
-				    var target = pi.ClientState.Targets.CurrentTarget;
-				    if (target is BattleNpc { NameId: 541 } dummy && (uint)dummy.ActorId != 0xE000_0000)
-				    {
-					    if (Marshal.ReadByte(target.Address + offset) != 0)
-					    {
-						    ExecuteCommand(0x140, dummy.ActorId, 0, 0, 0);
-						    PluginLog.Information($"clear target dummy enmity {dummy.ActorId:X}");
-					    }
-				    }
-			    }
-		    }){ HelpMessage = "Reset target striking dummy enmity."});
-	    }
+		private delegate long ExecuteCommandDele(int id, int a1, int a2, int a3, int a4);
+		private ExecuteCommandDele ExecuteCommand;
 
-	    public string Name => "Reset dummy enmity command";
-    }
+		public void Dispose()
+		{
+			CommandManager.RemoveHandler("/resetenmity");
+			CommandManager.RemoveHandler("/resetenmityall");
+		}
+
+		public unsafe Plugin()
+		{
+			var scanText = SigScanner.ScanText("E8 ?? ?? ?? ?? 8D 43 0A");
+			ExecuteCommand = Marshal.GetDelegateForFunctionPointer<ExecuteCommandDele>(scanText);
+			PluginLog.Debug($"{nameof(ExecuteCommand)} +{(long)scanText - (long)Process.GetCurrentProcess().MainModule.BaseAddress:X} ");
+
+			CommandManager.AddHandler("/resetenmity", new CommandInfo(ResetTarget) { HelpMessage = "Reset target dummy's enmity." });
+			CommandManager.AddHandler("/resetenmityall", new CommandInfo(ResetAll) { HelpMessage = "Reset the enmity of all dummies." });
+
+			void ResetEnmity(int objectId)
+			{
+				PluginLog.Information($"resetting enmity {objectId:X}");
+				ExecuteCommand(0x140, objectId, 0, 0, 0);
+			}
+
+			void ResetTarget(string s, string s1)
+			{
+				var target = TargetManager.Target;
+				if (target is Character { NameId: 541 }) ResetEnmity((int)target.ObjectId);
+			}
+
+			void ResetAll(string s, string s1)
+			{
+				var addonByName = GameGui.GetAddonByName("_EnemyList", 1);
+				if (addonByName != IntPtr.Zero)
+				{
+					var addon = (AddonEnemyList*)addonByName;
+					var numArray = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.Instance()->GetUiModule()->RaptureAtkModule.AtkModule.AtkArrayDataHolder.NumberArrays[19];
+
+					for (var i = 0; i < addon->EnemyCount; i++)
+					{
+						var enemyObjectId = numArray->IntArray[8 + i * 5];
+						var enemyChara = CharacterManager.Instance()->LookupBattleCharaByObjectId(enemyObjectId);
+						if (enemyChara is null) continue;
+						if (enemyChara->Character.NameID == 541) ResetEnmity(enemyObjectId);
+					}
+				}
+			}
+		}
+		public string Name => "Reset enmity command";
+	}
 }
